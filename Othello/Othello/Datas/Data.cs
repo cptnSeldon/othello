6c4da9312535afd0a3c6d930410039093feac2b7
axelroy <royaxe@gmail.com>
2017-01-06 16:57:30 +0100

    Adding the datas structures

---
+++ Othello/Othello/Datas/Data.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Othello
+{
+    // Outside of the class so it is accessible everywhere
+    public enum Colors { White, Black };
+
+    class Data
+    {
+
+        // Attributes
+        private Disc[,] board;
+        private OthelloEngine othelloEngine;
+
+        private int whiteScore;
+        private int whiteRemainingTime;
+        private int whiteRemainingDiscs;
+
+        private int blackRemainingDiscs;
+        private int blackRemainingTime;
+        private int blackScore;
+
+        public Data()
+        {
+            board = new Disc[8, 8];
+            whiteScore = 0;
+            whiteRemainingDiscs = 32;
+            whiteRemainingTime = 1000;
+            blackScore = 0;
+            blackRemainingDiscs = 32;
+            blackRemainingTime = 1000;
+        }
+
+    }
+}
---
+++ Othello/Othello/Datas/Disc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Othello
+{
+    class Disc
+    {
+        private Colors colors;
+
+        public Colors Colors
+                {
+                    get { return colors; }
+                    set { colors = value; }
+                }
+
+        /*
+                // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+                public static readonly DependencyProperty MyPropertyProperty =
+                    DependencyProperty.Register("MyProperty", typeof(int), typeof(ownerclass), new PropertyMetadata(0));
+        */
+    }
+}
