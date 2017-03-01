5bf4d200a813dd77d3d11f660faee1741211f7a6
axelroy <royaxe@gmail.com>
2017-03-01 23:03:21 +0100

    Version fonctionnelle en ligne de commande mais non intégrable au testeur pour le moment.

---
+++ Othello_IA/AlphaBeta_IA/AlphaBeta_IA/OthelloIA8.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Project references :
+/// 
+/// 
+/// </summary>
+namespace OthelloIA8
+{
+    public class AlphaBetaBoard : IPlayable.IPlayable
+    {
+        #region attributes
+       
+        private Data board;
+
+        public AlphaBetaBoard()
+        {
+            board = new Data(false);
+        }
+
+        #endregion
+
+        #region public methods
+        public Tuple<int, int> GetNextMove(int[,] game, int level, bool whiteTurn)
+        {
+            this.board = new Data(whiteTurn, game);
+
+            Dictionary<String, List<Tuple<int, int>>> possibleMoves = board.getPossibleMoves();
+            string key = "00";
+
+            foreach (KeyValuePair<string, List<Tuple<int, int>>> entry in possibleMoves)
+            {
+                //TODO : Travail correct à faire ici
+                Console.WriteLine(entry.Key);
+                key = entry.Key;
+            }
+
+            return board.stringToTuple(key);
+        }
+
+        public bool IsPlayable(int column, int line, bool isWhite)
+        {
+            return board.isPlayable(column, line, isWhite);
+        }
+
+        public bool PlayMove(int column, int line, bool isWhite)
+        {
+            return board.playMove(column, line, isWhite);
+        }
+
+        public int[,] GetBoard()
+        {
+            return this.board.getBoard();
+        }
+
+        public string GetName()
+        {
+            return "8: Nemeth_Roy";
+        }
+        #endregion
+
+
+        #region private methods
+
+
+        #endregion
+
+        #region score
+        public int GetBlackScore()
+        {
+            return 0;
+        }
+
+        public int GetWhiteScore()
+        {
+            return 0;
+        }
+        #endregion
+    }
+}
--- Othello_IA/AlphaBeta_IA/AlphaBeta_IA/Properties/AssemblyInfo.cs
+++ Othello_IA/AlphaBeta_IA/AlphaBeta_IA/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AlphaBeta_IA")]
+[assembly: AssemblyTitle("OthelloIA8")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AlphaBeta_IA")]
+[assembly: AssemblyProduct("OthelloIA8")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
